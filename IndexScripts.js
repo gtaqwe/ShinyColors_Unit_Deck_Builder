c26fd1cb41e42df47c2ab934d0986b67f85c0e6a
ZEED <gtaqwe@gmail.com>
2021-07-21 23:28:05 +0900

    Add Comment, View the card select dialog after selecting a character

--- IndexScripts.js
+++ IndexScripts.js
@@ -13,45 +13,46 @@ $().ready(function () {
 });
 
 async function init() {
+  // Read JSON
   await getJSON("json/data.json").then(function (resp) {
     jsonData = JSON.parse(resp);
   });
 
   // Produce Deck
   $("#idolCharBtn_Produce").click(function () {
-    viewIdol(this, jsonData, "selectedIdolCharViewDiv_Produce", "P", 0);
+    viewIdolDialog(this, jsonData, "selectedIdolCharViewDiv_Produce", "P", 0);
   });
   $("#idolCharBtn_Support_1").click(function () {
-    viewIdol(this, jsonData, "selectedIdolCharViewDiv_Support_1", "S", 1);
+    viewIdolDialog(this, jsonData, "selectedIdolCharViewDiv_Support_1", "S", 1);
   });
   $("#idolCharBtn_Support_2").click(function () {
-    viewIdol(this, jsonData, "selectedIdolCharViewDiv_Support_2", "S", 2);
+    viewIdolDialog(this, jsonData, "selectedIdolCharViewDiv_Support_2", "S", 2);
   });
   $("#idolCharBtn_Support_3").click(function () {
-    viewIdol(this, jsonData, "selectedIdolCharViewDiv_Support_3", "S", 3);
+    viewIdolDialog(this, jsonData, "selectedIdolCharViewDiv_Support_3", "S", 3);
   });
   $("#idolCharBtn_Support_4").click(function () {
-    viewIdol(this, jsonData, "selectedIdolCharViewDiv_Support_4", "S", 4);
+    viewIdolDialog(this, jsonData, "selectedIdolCharViewDiv_Support_4", "S", 4);
   });
   $("#idolCharBtn_Support_5").click(function () {
-    viewIdol(this, jsonData, "selectedIdolCharViewDiv_Support_5", "S", 5);
+    viewIdolDialog(this, jsonData, "selectedIdolCharViewDiv_Support_5", "S", 5);
   });
 
   // Fes Deck
   $("#idolCharBtn_Leader").click(function () {
-    viewIdol(this, jsonData, "selectedIdolCharViewDiv_Leader", "F", 0);
+    viewIdolDialog(this, jsonData, "selectedIdolCharViewDiv_Leader", "F", 0);
   });
   $("#idolCharBtn_Vocal").click(function () {
-    viewIdol(this, jsonData, "selectedIdolCharViewDiv_Vocal", "F", 1);
+    viewIdolDialog(this, jsonData, "selectedIdolCharViewDiv_Vocal", "F", 1);
   });
   $("#idolCharBtn_Center").click(function () {
-    viewIdol(this, jsonData, "selectedIdolCharViewDiv_Center", "F", 2);
+    viewIdolDialog(this, jsonData, "selectedIdolCharViewDiv_Center", "F", 2);
   });
   $("#idolCharBtn_Dance").click(function () {
-    viewIdol(this, jsonData, "selectedIdolCharViewDiv_Dance", "F", 3);
+    viewIdolDialog(this, jsonData, "selectedIdolCharViewDiv_Dance", "F", 3);
   });
   $("#idolCharBtn_Visual").click(function () {
-    viewIdol(this, jsonData, "selectedIdolCharViewDiv_Visual", "F", 4);
+    viewIdolDialog(this, jsonData, "selectedIdolCharViewDiv_Visual", "F", 4);
   });
 
   var fesChk = $("#fesImgConvertBtn").is(":checked");
@@ -64,6 +65,9 @@ async function init() {
   setDeckSpace("F", fDeckSpaceVal);
 }
 
+/**
+ * JSON 데이터 취득
+ */
 function getJSON(jsonFile) {
   try {
     return new Promise(function (resolve, reject) {
@@ -87,7 +91,11 @@ function getJSON(jsonFile) {
   }
 }
 
-function viewIdol(parentObj, obj, divId, cardType, offset) {
+/**
+ * 아이돌 선택 버튼 클릭시, 각 아이돌의 이미지를 보여주는 다이얼로그 표시
+ * 히라메키 표시가 체크 된 경우, 서포터 아이돌 목록에 히라메키 아이콘도 표시
+ */
+function viewIdolDialog(parentObj, obj, divId, cardType, offset) {
   $(idolDialogDivId).html("");
   for (var idolIdx = 0; idolIdx < obj.length; idolIdx++) {
     var idolNameSrc = obj[idolIdx]["idol_en_name"].toLowerCase();
@@ -107,14 +115,18 @@ function viewIdol(parentObj, obj, divId, cardType, offset) {
       "', " +
       offset;
 
+    // 아이돌 목록 다이얼로그 표시
+    // 아이돌 선택시, 해당 아이돌의 카드 목록 전개하도록 세팅
     $(idolDialogDivId).append(
       $("<div>", {
         id: "idolDiv_" + idolNameSrc,
         style: "display:inline-flex;position: relative;",
-        onclick: "setCardList(" + parameter + ")",
+        onclick: "viewCard(" + parameter + ")",
+        // onclick: "setCardList(" + parameter + ")",
       })
     );
 
+    // 아이돌 이미지 세팅
     $("#idolDiv_" + idolNameSrc).append(
       $("<img>", {
         id: "idol_" + idolNameSrc,
@@ -125,11 +137,14 @@ function viewIdol(parentObj, obj, divId, cardType, offset) {
       })
     );
 
+    // 히라메키 표시가 체크되어있는 경우
+    // 서포터 아이돌 목록에 히라메키 아이콘을 표시
     var insightChk = $("#insightConvertBtn").is(":checked");
     if (cardType == "S" && insightChk == true)
-      $("#idolDiv_" + idolNameSrc).append($("<img>", pasteInsightImg(idolInsight)));
+      $("#idolDiv_" + idolNameSrc).append($("<img>", getInsightImg(idolInsight)));
   }
 
+  // 다이얼로그 세팅
   $(idolDialogDivId).dialog({
     modal: true,
     title: "아이돌 선택",
@@ -142,7 +157,10 @@ function viewIdol(parentObj, obj, divId, cardType, offset) {
   });
 }
 
-function pasteIdolImg(name, insight) {
+/**
+ * 표시할 아이돌의 이미지와 히라메키 정보 Return
+ */
+function getIdolImg(name, insight) {
   return {
     name: name,
     insight: insight,
@@ -152,33 +170,68 @@ function pasteIdolImg(name, insight) {
   };
 }
 
-function pasteInsightImg(insight) {
+/**
+ * 히라메키 이미지 Return
+ */
+function getInsightImg(insight) {
   return {
     src: "./img/assets/" + insight + "_Insight.png",
     style: "position: absolute; width: 35px; height: 35px; bottom: 0px; right: 0px;",
   };
 }
 
+/**
+ * 아이돌 선택시 해당 아이돌의 카드를 표시하도록 세팅 후
+ * 선택 할 수 있도록 카드 다이얼로그를 전개
+ */
+function viewCard(divId, name, insight, idolIdx, cardType, offset) {
+  // 1. 먼저 선택한 아이돌의 카드를 세팅
+  setCardList(divId, name, insight, idolIdx, cardType, offset);
+
+  // 2. 세팅 후 자동으로 카드 선택 버튼을 클릭 동작을 행한다
+  if (cardType == "P") {
+    $("#idolCardBtn_Produce").click();
+  } else if (cardType == "S") {
+    $("#idolCardBtn_Support_" + offset).click();
+  } else {
+    $("#idolCardBtn_" + FES_POSITION[offset]).click();
+  }
+}
+
+/**
+ * 아이돌 선택시 카드 선택 버튼의 동작을 세팅
+ * 카드 종류는 (프로듀스, 서포트, 페스) 3종류로 처리
+ *
+ * 1. 각 카드 선택 버튼마다 선택한 아이돌 카드를 세팅 후
+ * 2. 숨겨놓았던 카드 선택 버튼을 표시
+ * 3. 히라메키 표시 설정에 따라 선택한 서포터 아이돌 칸(다이얼로그가 아님)에 히라메키 아이콘을 표시
+ */
 function setCardList(divId, name, insight, idolIdx, cardType, offset) {
   var selDivId = divId;
-  $(selDivId).html($("<img>", pasteIdolImg(name, insight)));
+  $(selDivId).html($("<img>", getIdolImg(name, insight)));
   $(idolDialogDivId).dialog("close");
 
+  // 프로듀스 카드의 경우
   if (cardType == "P") {
     $("#idolCardBtn_Produce").click(function () {
       viewCardDialog(this, jsonData[idolIdx], cardType, offset);
     });
     $("#idolCardBtn_Produce").css("visibility", "visible");
-  } else if (cardType == "S") {
+  }
+  // 서포터 카드의 경우
+  else if (cardType == "S") {
     $("#idolCardBtn_Support_" + offset).click(function () {
       viewCardDialog(this, jsonData[idolIdx], cardType, offset);
     });
     $("#idolCardBtn_Support_" + offset).css("visibility", "visible");
 
     var insightChk = $("#insightConvertBtn").is(":checked");
-    if (insightChk == true)
-      $("#selectedIdolCharViewDiv_Support_" + offset).append($("<img>", pasteInsightImg(insight)));
-  } else {
+    if (insightChk == true) {
+      $("#selectedIdolCharViewDiv_Support_" + offset).append($("<img>", getInsightImg(insight)));
+    }
+  }
+  // 페스 카드의 경우
+  else {
     $("#idolCardBtn_" + FES_POSITION[offset]).click(function () {
       viewCardDialog(this, jsonData[idolIdx], cardType, offset);
     });
@@ -186,17 +239,24 @@ function setCardList(divId, name, insight, idolIdx, cardType, offset) {
   }
 }
 
+/**
+ * 카드 선택 버튼 클릭시 각 아이돌의 카드 이미지를 보여주는 다이얼로그를 표시
+ */
 function viewCardDialog(parentObj, obj, cardType, offset) {
   var type_list;
   var divOffset;
   var imgPath;
 
+  // 1. 카드가 프로듀스(P), 서포트(S), 페스(F) 카드 종류에 따라 표시하는 카드의 등급을 확인
+  // P, F : P_SSR, P_SR, P_R : 3종류
+  // S : S_SSR, S_SR, S_R, S_N : 4종류
   if (cardType == "P" || cardType == "F") {
     type_list = P_TYPE_LIST;
   } else {
     type_list = S_TYPE_LIST;
   }
 
+  // 2. 카드의 위치(포지션)와 이미지 경로를 설정 (페스 카드의 경우, 사복과 페스 설정에 따라 경로 설정)
   if (cardType == "P" || cardType == "S") {
     divOffset = offset;
     imgPath = "card/";
@@ -207,6 +267,7 @@ function viewCardDialog(parentObj, obj, cardType, offset) {
     else imgPath = "card/";
   }
 
+  // 3. 카드 목록 다이얼로그 표시
   $(cardDialogDivId).html("");
   for (var typeIdx = 0; typeIdx < type_list.length; typeIdx++) {
     if (obj["card_data"][type_list[typeIdx]] == undefined) continue;
@@ -217,7 +278,10 @@ function viewCardDialog(parentObj, obj, cardType, offset) {
     $(cardDialogDivId).append(
       "<h3 style='margin: 10px 0px'>" + type_list[typeIdx].split("_")[1] + "</h3>"
     );
+
     for (var i = 0; i < cardLen; i++) {
+      // 카드 목록의 이미지 설정
+      // 카드 이미지가 없는 경우 「Blank_Idol.png」을 표시
       $(cardDialogDivId).append(
         $("<img>", {
           id: cardList[i]["card_addr"],
@@ -228,25 +292,22 @@ function viewCardDialog(parentObj, obj, cardType, offset) {
           onerror: "this.src='./img/assets/Blank_Idol.png'",
         })
       );
+
+      // 카드 선택시 덱에 입력
       $("#" + cardList[i]["card_addr"]).click(function () {
         var selDivId = "#selectedIdolView_" + divOffset;
-        $(selDivId).html(
-          $("<img>", {
-            src: "./img/" + imgPath + this.id + ".png",
-            width: "96px",
-            height: "96px",
-            onerror: "this.src='./img/assets/Blank_Idol.png'",
-          })
-        );
+        setSelectCard(selDivId, imgPath, this.id);
         $(cardDialogDivId).dialog("close");
       });
     }
 
+    // 각 등급별 구분 라인 추가
     if (typeIdx + 1 < type_list.length) {
       $(cardDialogDivId).append("<hr>");
     }
   }
 
+  // 다이얼로그 세팅
   $(cardDialogDivId).dialog({
     modal: true,
     title: "카드 선택",
@@ -259,11 +320,31 @@ function viewCardDialog(parentObj, obj, cardType, offset) {
   });
 }
 
+/**
+ * 카드 선택시 해당되는 위치에 카드 이미지 설정
+ */
+function setSelectCard(divId, imgPath, cardAddr) {
+  $(divId).html(
+    $("<img>", {
+      src: "./img/" + imgPath + cardAddr + ".png",
+      width: "96px",
+      height: "96px",
+      onerror: "this.src='./img/assets/Blank_Idol.png'",
+    })
+  );
+}
+
+/**
+ * 페스덱의 페스이미지와 사복 이미지의 설정
+ */
 function convertFesImg(fesChk) {
   getToggleString(fesChk);
   convertFesDeckImg(fesChk);
 }
 
+/**
+ * 페스와 사복 토글의 텍스트 표시
+ */
 function getToggleString(fesChk) {
   if (fesChk == true) {
     $("#toggleStr").html("페스");
@@ -272,6 +353,9 @@ function getToggleString(fesChk) {
   }
 }
 
+/**
+ * 페스와 사복 아이콘 표시 설정에 따라 이미지를 변경
+ */
 function convertFesDeckImg(fesChk) {
   for (var i = 0; i < FES_POSITION.length; i++) {
     var imgUrl = $("#selectedIdolView_" + FES_POSITION[i])
@@ -290,6 +374,9 @@ function convertFesDeckImg(fesChk) {
   }
 }
 
+/**
+ * 서포터 아이돌 이미지에 히라메키 아이콘 표시 설정
+ */
 function convertInsightImg(insightChk) {
   for (var i = 0, offset = 1; i < 5; i++, offset++) {
     var imgCode = $("#selectedIdolCharViewDiv_Support_" + offset).children("img");
@@ -301,20 +388,21 @@ function convertInsightImg(insightChk) {
       $("#selectedIdolCharViewDiv_Support_" + offset).html("");
       if (insightChk == true) {
         $("#selectedIdolCharViewDiv_Support_" + offset).append(
-          $("<img>", pasteIdolImg(idolNameSrc, insight))
+          $("<img>", getIdolImg(idolNameSrc, insight))
         );
-        $("#selectedIdolCharViewDiv_Support_" + offset).append(
-          $("<img>", pasteInsightImg(insight))
-        );
+        $("#selectedIdolCharViewDiv_Support_" + offset).append($("<img>", getInsightImg(insight)));
       } else {
         $("#selectedIdolCharViewDiv_Support_" + offset).append(
-          $("<img>", pasteIdolImg(idolNameSrc, insight))
+          $("<img>", getIdolImg(idolNameSrc, insight))
         );
       }
     }
   }
 }
 
+/**
+ * 덱 이미지 표시 간격 설정
+ */
 function setDeckSpace(deckType, spaceType) {
   var spaceAry;
 
@@ -338,6 +426,9 @@ function setDeckSpace(deckType, spaceType) {
 
 //////////////////////////////////////////////////
 
+/**
+ * 이미지 캡쳐 다운로드
+ */
 function captureScreen(frameId, fName, scale) {
   var captureName = `${fName}_Deck_x${scale}.png`;
 
