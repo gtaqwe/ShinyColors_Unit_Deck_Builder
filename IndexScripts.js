c06fd9212fe5f1dda055cadad643310923b8c1aa
ZEED <gtaqwe@gmail.com>
2021-07-22 18:46:26 +0900

    Add URL Copy

--- IndexScripts.js
+++ IndexScripts.js
@@ -198,6 +198,27 @@ function getFesDeckUrl() {
 }
 
 /**
+ * Div의 텍스트를 클립보드에 복사
+ */
+function CopyToClipboard(containerid) {
+  // Create a new textarea element and give it id='temp_element'
+  const textarea = document.createElement("textarea");
+  textarea.id = "temp_element";
+  // Optional step to make less noise on the page, if any!
+  textarea.style.height = 0;
+  // Now append it to your page somewhere, I chose <body>
+  document.body.appendChild(textarea);
+  // Give our textarea a value of whatever inside the div of id=containerid
+  textarea.value = document.getElementById(containerid).innerText;
+  // Now copy whatever inside the textarea to clipboard
+  const selector = document.querySelector("#temp_element");
+  selector.select();
+  document.execCommand("copy");
+  // Remove the textarea
+  document.body.removeChild(textarea);
+}
+
+/**
  * JSON 데이터 취득
  */
 function getJSON(jsonFile) {
